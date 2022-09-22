f83108f98a2f5fe1478d7eb7179c742c46ea1671
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2022-09-22 10:00:52 +0530

    Code added

--- 1_Basics/Code.go
+++ 1_Basics/Code.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//Declaration of variables
+	//1.
+	var a int = 10
+	//2.
+	const b int = 10
+	//3.
+	c := 4
+	fmt.Println(a, b, c)
 }
