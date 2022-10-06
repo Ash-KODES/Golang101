b4d5b90413a414030962b50a1ac2c505eae19788
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2022-10-06 11:38:24 +0530

    Case styles added

---
+++ 8_File_naming_convention_knyo/code.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// Case Style in programming--->Need to follow a single convention throughout the code
+func main() {
+	fmt.Println("camelCase")
+	fmt.Println("snake_case")
+	fmt.Println("kebab-case")
+	fmt.Println("PascalCase")
+}
