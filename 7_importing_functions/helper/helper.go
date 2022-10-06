b933dd22a421b949527eccb8c661fe37cf7ab9cb
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2022-10-06 10:40:53 +0530

    Importin fucntions from different package

---
+++ 7_importing_functions/helper/helper.go
@@ -0,0 +1,7 @@
+package helper
+
+func Helper() string {
+	return "I am function from helper package"
+}
+
+//Function that is to be imported should have name starting with capital letter.
---
+++ 7_importing_functions/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"glabbr.tech/Go101/7_importing_functions/helper"
+)
+
+// import path of function is to be written.""glabbr.tech/Go101/7_importing_functions/helper""
+func main() {
+	fmt.Println(helper.Helper())
+}
